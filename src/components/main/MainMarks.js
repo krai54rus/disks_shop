7c0816403b8e1f1da3fadcbba78888ab8557992d
krai54rus <krai54rus@gmail.com>
2021-05-04 20:37:49 +0700

    main page in process

--- src/components/main/MainMarks.js
+++ src/components/main/MainMarks.js
@@ -1,9 +1,24 @@
 import React from 'react';
-function MainMarks(){
+function MainMarks(props){
     return (
         <div className="main-marks">
-                <div className="main-banner__img">
-                    <img src="./img/banner.jpg" className="" alt="" />
+            <div className="main-marks__title">
+                <span>Диски для популярных моделей</span>
+            </div>
+                <div className="main-marks__list">
+                    {
+                        props.marks.length > 0 &&
+                        props.marks.map((item,index)=>
+                            <div key={index} className="main-marks__list_item">
+                                <div className="main-marks__list_item-img">
+                                    <img src={`./img/${item.img}`} alt=""/>
+                                </div>
+                                <div className="main-marks__list_item-text">
+                                    <span>Диски для {item.name}</span>
+                                </div>
+                            </div>
+                        )
+                    }
                 </div>
         </div>
     );
--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import MainBanner from '../main/MainBanner';
+import MainMarks from '../main/MainMarks';
 function MainPage(){
+    // useEffect(() => {
+    //     // effect
+    //     fetch('/testapi/marks.json')
+    //     .then(res=>res.json())
+    //     .then(res=>console.log(res));
+    //     // return () => {
+    //     //     cleanup
+    //     // };
+    // }, [input]);
+    const [markArr,getMarks] = useState([]);
+    useEffect(() => {
+        console.log('use effect Main page');
+        if (markArr.length === 0) {
+            fetch('./models.json')
+            .then(res=>res.json())
+            .then(res=>{
+                getMarks(res);
+                console.log(res);
+            });
+            // .catch(err => {
+            //     console.log(err)
+            // };
+        };
+    })
     return(
         <div className="main-page">
             <MainBanner />
+            <MainMarks marks={markArr} />
             {/* 
-            <MainMarks />
             <MainDisks />
             <MainBrands /> */}
         </div>
